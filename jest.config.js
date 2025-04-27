26ee9a53eac708c816ae7b9b7ec2a98145a5e5ae
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-04-27 16:24:47 -0300

    adiciona jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',  // Configura o Jest para usar babel-jest para transformar arquivos JS/JSX
+  },
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],  // Se você estiver utilizando o setup-tests.js para configuração extra
+  testEnvironment: 'jest-environment-jsdom',  // Configura o ambiente de testes para o DOM
+}
