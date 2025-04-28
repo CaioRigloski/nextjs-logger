f4ca68062d4138b416048748be4c359e5997d6f3
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-04-27 22:13:22 -0300

    altera jest config para mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,6 +2,6 @@ export default {
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',  // Configura o Jest para usar babel-jest para transformar arquivos JS/JSX
   },
-  setupFilesAfterEnv: ['./setup-tests.js'],  // Se você estiver utilizando o setup-tests.js para configuração extra
+  setupFilesAfterEnv: ['./setup-tests.mjs'], 
   testEnvironment: 'jest-environment-jsdom',  // Configura o ambiente de testes para o DOM
 }
